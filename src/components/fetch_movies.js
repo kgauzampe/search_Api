34f475ecf523d651f94e348294e091860fe4ba6e
kgauzampe <kgauza.mpe@gmail.com>
2020-06-19 19:29:26 +0200

    files created

---
+++ src/components/fetch_movies.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export default function Fetch() {
+
+
+    state = {
+        loading: true
+    }
+componentDidMount() {
+
+
+}
+
+
+  render(); {
+      return (
+        {this.state.loading ? <div>loading</div> : <div>Movies</div>}
+      );
+  }
+}
+
+
